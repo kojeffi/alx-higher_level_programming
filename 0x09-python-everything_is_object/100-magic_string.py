1930f2e8897f934cf49f3b4111468b6d58cc2f51
kojeffi <omondijeff88@gmail.com>
2022-10-18 14:34:16 -0700

    a function magic_string() that returns a string “BestSchool” n times the number of the iteration

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-
-"""Defines a locked class."""
-
-
-class LockedClass:
-    """
-    Prevent the user from instantiating new LockedClass attributes
-    for anything but attributes called 'first_name'.
-    """
-
-    __slots__ = ["first_name"]
+def magic_string():
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".join(["BestSchool" for i in range(magic_string.count)])
