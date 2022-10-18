af7f74eb8701b5ce3d75dcdf2ab3e93476e5169f
kojeffi <omondijeff88@gmail.com>
2022-10-18 00:57:49 -0700

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """solves the N-queens puzzle"""
 
-
-
 import sys
-
 
 
 def init_board(n):
     """Initialize an `n`x`n` sized chessboard with 0's."""
-
     board = []
     [board.append([]) for i in range(n)]
     [row.append(' ') for i in range(n) for row in board]
